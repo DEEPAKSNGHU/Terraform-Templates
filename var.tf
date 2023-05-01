13ad9305783b23f938767cde987edd257a3cdf23
unknown <deepaksingh189189@gmail.com>
2023-05-01 15:59:33 +0530

    adding var file

---
+++ var.tf
@@ -0,0 +1,26 @@
+variable "ami" {
+  type    = string
+  default = "ami-0bde1eb2c18cb2abe"
+
+}
+variable "instancetype" {
+  type    = string
+  default = "t2.micro"
+
+}
+variable "availabilityzone" {
+  type    = string
+  default = "us-east-1a"
+
+}
+variable "KeyName" {
+  type    = string
+  default = "punchu"
+
+}
+variable "iaminstanceprofile" {
+  type    = string
+  default = "TestingRole"
+
+}
+
