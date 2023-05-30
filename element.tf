6ada5c9d67ecf679a7782a1c0e53b92931b3d98c
DEEPAKSNGHU <129820466+DEEPAKSNGHU@users.noreply.github.com>
2023-05-30 18:22:28 +0530

    Update element.tf

--- element.tf
+++ element.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "element" {
     instance_type = var.instancetype
     key_name = var.keyname
     availability_zone = "${element(var.availabilityzone, count.index)}"
-    count = 3
+    count = 1
   iam_instance_profile =  var.iaminstance
   tags = {
     Name = "element-${count.index+1}"
